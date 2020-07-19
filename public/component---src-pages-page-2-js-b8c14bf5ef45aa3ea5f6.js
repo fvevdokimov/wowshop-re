7f58b15ae6039c0996705d44442c4669d17c7395
fvevdokimov <fvevdokimov@gmail.com>
2020-07-19 20:39:15 +0700

    'ololol'

---
+++ public/component---src-pages-page-2-js-b8c14bf5ef45aa3ea5f6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{p5nM:function(e,t,a){"use strict";a.r(t);var l=a("FdF9"),n=a("Wbzz"),o=a("Bl7J"),u=a("vrFN");t.default=function(){return l.default.createElement(o.a,null,l.default.createElement(u.a,{title:"Page two"}),l.default.createElement("h1",null,"Hi from the second page"),l.default.createElement("p",null,"Welcome to page 2"),l.default.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b8c14bf5ef45aa3ea5f6.js.map
